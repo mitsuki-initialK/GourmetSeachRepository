ec22c4941fe04d64592fcac7dc1b305177f28576
initialK <w10tkmks0829@outlook.jp>
2024-04-19 01:17:20 +0900

    完成版

--- app/src/main/java/com/example/gourmetsearchapp/ui/GourmetSearchApp.kt
+++ app/src/main/java/com/example/gourmetsearchapp/ui/GourmetSearchApp.kt
@@ -1,8 +1,15 @@
 package com.example.gourmetsearchapp.ui
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -12,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -20,17 +28,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.gourmetsearchapp.ui.screen.GourmetSearchViewModel
+import com.example.gourmetsearchapp.R
 import com.example.gourmetsearchapp.gourmetSearchAPI.Restaurant
 import com.example.gourmetsearchapp.ui.screen.DetailScreen
+import com.example.gourmetsearchapp.ui.screen.GourmetSearchViewModel
 import com.example.gourmetsearchapp.ui.screen.ResultScreen
 import com.example.gourmetsearchapp.ui.screen.SearchScreen
+
 
 enum class GourmetSearchScreen(@StringRes val title : Int) {
     Search(title = 1),
@@ -47,21 +62,39 @@ fun GourmetSearchAppBar(
 ) {
 
     TopAppBar(
-        title = { "TOP APP BAR" },
+        title = {
+            Box(modifier = Modifier.fillMaxWidth().height(56.dp)) {
+                if (canNavigateBack) {
+                    IconButton(onClick = navigateUp, modifier = Modifier.fillMaxHeight()) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "backButton"
+                        )
+                    }
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_restaurant),
+                        modifier = Modifier.size(32.dp),
+                        contentDescription = ""
+                    )
+                    Text(
+                        text = "Restaurant Search",
+                        fontSize = 28.sp,
+                        color = Color.White
+                    )
+                }
+            }
+        },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
         ),
         modifier = modifier,
-        navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "backButton"
-                    )
-                }
-            }
-        }
     )
 }
 
